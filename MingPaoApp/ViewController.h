853f0a70b26b181487bae42e97b2e0d1cf54a6a7
jian <mingpao@mingpaos-Mac-mini.local>
2014-05-20 10:17:35 +0800

    Initial Commit

---
+++ MingPaoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MingPaoApp
+//
+//  Created by jian on 5/20/14.
+//  Copyright (c) 2014 jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
